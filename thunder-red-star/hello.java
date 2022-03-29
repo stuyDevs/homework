9cd0230f2367f739628c1a42bf655b7acbe64774
Salaj Rijal <60166498+srijal30@users.noreply.github.com>
2022-03-29 09:08:29 -0400

    someones hw

---
+++ thunder-red-star/hello.java
@@ -0,0 +1,295 @@
+thunder-red-star/hello.java
+// hello world + test
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    // print the sum of two numbers
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    // print the sum of three numbers
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    // print the sum of four numbers
+    public static int sum(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    // print the sum of five numbers
+    public static int sum(int a, int b, int c, int d, int e) {
+        return a + b + c + d + e;
+    }
+
+    // print the sum of six numbers
+    public static int sum(int a, int b, int c, int d, int e, int f) {
+        return a + b + c + d + e + f;
+    }
+
+    // print the sum of seven numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g) {
+        return a + b + c + d + e + f + g;
+    }
+
+    // print the sum of eight numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h) {
+        return a + b + c + d + e + f + g + h;
+    }
+
+    // print the sum of nine numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i) {
+        return a + b + c + d + e + f + g + h + i;
+    }
+
+    // print the sum of ten numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j) {
+        return a + b + c + d + e + f + g + h + i + j;
+    }
+
+    // print the sum of eleven numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k) {
+        return a + b + c + d + e + f + g + h + i + j + k;
+    }
+
+    // print the sum of twelve numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l) {
+        return a + b + c + d + e + f + g + h + i + j + k + l;
+    }
+
+    // print the sum of thirteen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m;
+    }
+
+    // print the sum of fourteen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n;
+    }
+
+    // print the sum of fifteen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o;
+    }
+
+    // print the sum of sixteen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p;
+    }
+
+    // print the sum of seventeen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q;
+    }
+
+    // print the sum of eighteen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r;
+    }
+
+    // print the sum of nineteen numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s;
+    }
+
+    // print the sum of twenty numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t;
+    }
+
+    // print the sum of twenty-one numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u;
+    }
+
+    // print the sum of twenty-two numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v;
+    }
+
+    // print the sum of twenty-three numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w;
+    }
+
+    // print the sum of twenty-four numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w + x;
+    }
+
+    // print the sum of twenty-five numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w + x + y;
+    }
+
+    // print the sum of twenty-six numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y, int z) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w + x + y + z;
+    }
+
+    // print the sum of twenty-seven numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y, int z, int aa) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w + x + y + z + aa;
+    }
+
+    // print the sum of twenty-eight numbers
+    public static int sum(int a, int b, int c, int d, int e, int f, int g, int h, int i, int j, int k, int l, int m, int n, int o, int p, int q, int r, int s, int t, int u, int v, int w, int x, int y, int z, int aa, int bb) {
+        return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q + r + s + t + u + v + w + x + y + z + aa + bb;
+    }
+
+    // Print a string of the numbers from 1 to 100
+    public static void print1to100() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+
+    // Print the answer to life, the universe, and everything
+    public static void printAnswer() {
+        System.out.println("42");
+    }
+
+    // GET a URL from the web and print it to the console
+    public static void printURL() {
+        try {
+            URL url = new URL("http://www.google.com");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Print the name of the best programming language
+    public static void printLanguage() {
+        System.out.println("Java");
+    }
+
+    // Print the name of the worst programming language
+    public static void printWorstLanguage() {
+        System.out.println("C");
+    }
+
+    // Print the name of the second best programming language
+    public static void printSecondLanguage() {
+        System.out.println("Python");
+    }
+
+    // Print the name of the second worst programming language
+    public static void printSecondWorstLanguage() {
+        System.out.println("C++");
+    }
+
+    // Print the name of the third best programming language
+    public static void printThirdLanguage() {
+        System.out.println("Ruby");
+    }
+
+    // Print the name of the third worst programming language
+    public static void printThirdWorstLanguage() {
+        System.out.println("JavaScript");
+    }
+
+    // Print the name of the fourth best programming language
+    public static void printFourthLanguage() {
+        System.out.println("C#");
+    }
+
+    // Print the name of the fourth worst programming language
+    public static void printFourthWorstLanguage() {
+        System.out.println("C");
+    }
+
+    // Print the name of the fifth best programming language
+    public static void printFifthLanguage() {
+        System.out.println("Go");
+    }
+
+    // Print the name of the fifth worst programming language
+    public static void printFifthWorstLanguage() {
+        System.out.println("Python");
+    }
+
+    // Print the name of the sixth best programming language
+    public static void printSixthLanguage() {
+        System.out.println("JavaScript");
+    }
+
+    // Print the name of the sixth worst programming language
+    public static void printSixthWorstLanguage() {
+        System.out.println("C");
+    }
+
+    // Print the name of the seventh best programming language
+    public static void printSeventhLanguage() {
+        System.out.println("Java");
+    }
+
+    // Print the name of the seventh worst programming language
+    public static void printSeventhWorstLanguage() {
+        System.out.println("C++");
+    }
+
+    // Print every character in the UTF-8 encoding
+    public static void printUTF8() {
+        for (int i = 0; i < 256; i++) {
+            System.out.print((char) i + " ");
+        }
+    }
+
+    // Print every character in the UTF-16 encoding
+    public static void printUTF16() {
+        for (int i = 0; i < 65536; i++) {
+            System.out.print((char) i + " ");
+        }
+    }
+
+    // Print every character in the UTF-32 encoding
+    public static void printUTF32() {
+        for (int i = 0; i < 1114112; i++) {
+            System.out.print((char) i + " ");
+        }
+    }
+
+    // Tell unix to shut up
+    public static void shutUp() {
+        System.out.println("shut up");
+    }
+
+    // Make a random syscall
+    public static void makeRandomSyscall() {
+        System.out.println("syscall");
+    }
+
+    // Find file drivers and copy them to working directory
+    public static void findFileDrivers() {
+        try {
+            File file = new File("/proc/filesystems");
+            BufferedReader in = new BufferedReader(new FileReader(file));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // uh
+    public static void uh() {
+        System.out.println("uh");
+    }
+}
